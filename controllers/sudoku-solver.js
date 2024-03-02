2d578936b55ba590b3c2b38a9047576aadba56b9
Dammy <bestbynature2010@gmail.com>
2024-03-02 18:02:11 +0100

    Update sudoku solver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,10 +1,17 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if(puzzleString.length !== 81) {
+      return { error: 'Expected puzzle to be 81 characters long' }
+    }
+    if(puzzleString.match(/[^0-9.]/)) {
+      return { error: 'Invalid characters in puzzle' }
+    }
+    return true
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    
   }
 
   checkColPlacement(puzzleString, row, column, value) {
